9ddfe64b1f0c5d6f715491779cd5f8e038ca3352
Arnab Chatterjee <arnabc1701@gmail.com>
2021-03-15 09:56:40 +0530

    chatengine added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import  {ChatEngine} from 'react-chat-engine';
+import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="d792075f-b60d-40c9-89b2-eabdb7c619e5"
+            userName="arn4b"
+            userSecret="arnab"
+        />
+    );
+}
+
+export default App;
